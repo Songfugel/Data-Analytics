de959ecad90fbda167560938dc145b95511f4bc3
Santtu <kahkonen@hotmail.com>
2022-05-11 14:37:18 +0300

    Update exercise2_1.py

--- Exercises/Exercise 2/exercise2_1.py
+++ Exercises/Exercise 2/exercise2_1.py
@@ -3,18 +3,29 @@
 import pandas as pd
 print('\nEXERCISES 2.1.x\n')
 
-COL_JOB_TITLE = 'Job Title'
 CSV_FILE = "loans.csv"
+
+COL_CUSTOMER_ID = "Customer ID"
+COL_CURRENT_LOAN_AMOUNT = 'Current Loan Amount'
+COL_ANNUAL_INCOME = 'Annual Income'
+COL_ACTUAL_ANNUAL_INCOME = "Actual Annual Income"
+COL_MONTHLY_DEBT = 'Monthly Debt'
+COL_LOAN_ID = 'Loan ID'
+COL_HOME_OWNERSHIP = 'Home Ownership'
+COL_TERM = 'Term'
+COL_BANKRUPTCIES = 'Bankruptcies'
+COL_PURPOSE = 'Purpose'
+COL_NUMBER_OF_OPEN_ACCOUNTS = 'Number of Open Accounts'
+COL_NUMBER_OF_CREDIT_PROBLEMS = 'Number of Credit Problems'
 
 df_loans = pd.read_csv(CSV_FILE)
 df = df_loans.copy()
-
 
 print(''' 
       ¤ ¤ ¤ ¤ ¤ ¤    EXERCISES 2.1.x    ¤ ¤ ¤ ¤ ¤ ¤''')
 
 ex = '''
-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+----------------------------------------------------------------------------
 
 Put all your exercises in your Git-project and give the repository address to 
 your instructor!
@@ -26,145 +37,163 @@ EXERCISE 2.1
 
 import pandas and read the csv-file found in Moodle (loans.csv). Use 
 Python coding with pandas to answer the questions.
-
-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
-
-'''
+----------------------------------------------------------------------------'''
 
 ex1 = '''
 ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 Exercise 1. Remove the Customer ID –column from data
 ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
 ex1__pre = df.copy()
-
-ex1_sol = df = df.drop("Customer ID", axis=1)
+ex1_sol = df = df.drop(COL_CUSTOMER_ID, axis=1)
 
 ex2 = '''
 ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 Exercise 2. Print the head of the data
 ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
-print(ex2)
-ex2_answer = df.head() 
+ex2__pre = df.copy()
+ex2_sol = df.head() 
 
 ex3 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 3. Remove rows from the data that have a too large of a loan
 (Current Loan Amount should be less than 99999999)
-'''
-print(ex3)
-ex3_answer = df = df[df['Current Loan Amount'] < 99999999]
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex3__pre = df.copy()
+ex3_sol = df = df[df[COL_CURRENT_LOAN_AMOUNT] < 99999999]
 
 ex4 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 4. Remove rows that have the annual income as NaN (not a number
-'''
-print(ex4)
-ex4_answer = df.dropna(subset=['Annual Income'])
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex4__pre = df.copy()
+ex4_sol = df.dropna(subset=[COL_ANNUAL_INCOME])
 
 ex4extra = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 4.Extra task: use imputation to use average income as the 
 value instead of NaN
-'''
-print(ex4extra)
-ex4extra_answer = df = df.fillna(df['Annual Income'].mean())
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex4extra_answer = df = df.fillna(df[COL_ANNUAL_INCOME].mean())
 
 ex5 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 5.  Get the average Current Loan Amount
-'''
-print(ex5)
-ex5_answer = round(df['Current Loan Amount'].mean(), 2)
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex5__pre = df.copy()
+ex5_sol = round(df[COL_CURRENT_LOAN_AMOUNT].mean(), 2)
 
 ex6 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 6. Get the a) highest and b) lowest Annual Income in the dataset
-'''
-print(ex6)
-ex6a_answer = round(df['Annual Income'].max(), 2)
-ex6b_answer = round(df['Annual Income'].min(), 2)
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex6__pre = df.copy()
+ex6a_sol = round(df[COL_ANNUAL_INCOME].max(), 2)
+ex6b_sol = round(df[COL_ANNUAL_INCOME].min(), 2)
 
 ex7 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 7. Note: The Actual Annual Income should be : Annual Income – 12 * Monthly Debt
 Tip: Create a new field with the above instruction to the DataFrame.
-'''
-print(ex7)
-ex7_answer = df["Actual Annual Income"] = df.apply(lambda x: x['Monthly Debt']*12, axis=1)
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex7__pre = df.copy()
+ex7_sol = df[COL_ACTUAL_ANNUAL_INCOME] = df.apply(
+    lambda x: x[COL_MONTHLY_DEBT]*12, axis=1)
 
 def getValue(id_field, id_value, column_name):
     row = df[df[id_field] == id_value]
     return row.iloc[0][column_name]
 
 ex8 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 8. Get the Home Ownership value of the 
 Loan ID = bbf87a87-22cd-4d10-bd9b-7a9cc1b6e59d
-'''
-print(ex8)
-ex8_answer = getValue('Loan ID',
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex8__pre = df.copy()
+ex8_sol = getValue(COL_LOAN_ID,
     'bbf87a87-22cd-4d10-bd9b-7a9cc1b6e59d',
-     'Home Ownership')
+     COL_HOME_OWNERSHIP)
 
 ex9 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 9. Get the Actual Annual Income of the loan with the ID = 
 76fa89b9-e6a8-49af-afa1-8151315aba8e
-'''
-print(ex9)
-ex9_answer = getValue('Loan ID',
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex9__pre = df.copy()
+ex9_sol = getValue(COL_LOAN_ID,
     '76fa89b9-e6a8-49af-afa1-8151315aba8e',
-     'Actual Annual Income')
-
+     COL_ACTUAL_ANNUAL_INCOME)
 
 ex10 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 10. Get the Loan ID of the loan with the smallest Actual Annual 
 Income
-'''
-print(ex10)
-ex10_answer = df.loc[df['Annual Income'].idxmin()]['Loan ID']
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex10__pre = df.copy()
+ex10_sol = df.loc[df[COL_ANNUAL_INCOME].idxmin()][COL_LOAN_ID]
 
 ex11 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 11. How many loans are "Long term"?
-'''
-print(ex11)
-ex11_answer = df[df['Term'] == 'Long Term'].count()[0]
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex11__pre = df.copy()
+ex11_sol = df[df[COL_TERM] == 'Long Term'].count()[0]
 
 ex12 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 12. How many loaners have more than 1 bankruptcy?
-'''
-print(ex12)
-ex12_answer = df[df['Bankruptcies'] > 1].count()[0]
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex12__pre = df.copy()
+ex12_sol = df[df[COL_BANKRUPTCIES] > 1].count()[0]
 
 ex13 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 13. How many Short Term loans are for Home Improvements
-'''
-print(ex13)
-ex13_answer = df[(df['Term'] == 'Short Term') & (df['Purpose'] == 'Home Improvements')].count()[0]
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex13__pre = df.copy()
+ex13_sol = df[(df[COL_TERM] == 'Short Term') & 
+                 (df[COL_PURPOSE] == 'Home Improvements')].count()[0]
 
 ex14 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 14. How many unique loan purposes are there?
-'''
-print(ex14)
-ex14_answer = len(df['Purpose'].unique())
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex14__pre = df.copy()
+ex14_sol = len(df[COL_PURPOSE].unique())
 
 ex15 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 15. What are the 3 most common loan purposes?
-'''
-print(ex15)
-ex15_answer = df.groupby('Purpose').count().nlargest(3, "Loan ID")
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex15__pre = df.copy()
+ex15_sol = df.groupby(COL_PURPOSE).count().nlargest(3, COL_LOAN_ID)
 
 ex16 = '''
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 16. Is there a correlation between a) Annual Income and Number of 
 Open Accounts or is there a correlation between b) Number of 
 Credit Problems and Bankruptcies
-'''
-print(ex16)
-ex16a_answer = df['Annual Income'].corr(df['Number of Open Accounts'])
-ex16a_answer_analysis =' 0.12 some, but not much correlation at all'
+¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤'''
+ex16__pre = df.copy()
+ex16a_sol = df[COL_ANNUAL_INCOME].corr(df[COL_NUMBER_OF_OPEN_ACCOUNTS])
+ex16a_analysis ='''
+----------------------------------------------------------------------------
+ANALYSIS: 0.12 some, but not much correlation at all'
+----------------------------------------------------------------------------'''
 
-ex16b_answer_orig = df['Number of Credit Problems'].corr(df['Bankruptcies'])
-ex16b_answer_orig_analysis = '''
-Shows no correlation at all... which seems completely against common sense
-.
-.
-.
-There seems to be some insane data in bankruptcies with millions of bankruptcies we need to remove'''
-ex16b_answer_crazy_rows = df[df['Bankruptcies'] > 10]
-df = df[df['Bankruptcies'] < 10]
+ex16b_sol_orig = df[COL_NUMBER_OF_CREDIT_PROBLEMS].corr(df[COL_BANKRUPTCIES])
+ex16b_sol_orig_analysis = '''
+----------------------------------------------------------------------------
+ANALYSIS:  Shows no correlation at all... which seems completely against
+common sense
 
-ex16b_answer_fixed = df['Number of Credit Problems'].corr(df['Bankruptcies'])
-ex16b_answer_fixed_analysis='''
-Now ~0.75 very strong correlation as expected'''
+There seems to be some insane data in bankruptcies with millions of
+bankruptcies we need to remove
+----------------------------------------------------------------------------'''
+ex16b_sol_crazy_rows = df[df[COL_BANKRUPTCIES] > 10]
+df = df[df[COL_BANKRUPTCIES] < 10]
+
+ex16b_sol_fixed = df[COL_NUMBER_OF_CREDIT_PROBLEMS].corr(df[COL_BANKRUPTCIES])
+ex16b_sol_fixed_analysis='''
+----------------------------------------------------------------------------
+ANALYSIS: Now ~0.75 very strong correlation as expected
+----------------------------------------------------------------------------'''
