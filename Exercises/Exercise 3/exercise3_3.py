57b7bbd902a8da3fe8c88baafdf85eb757296e98
Santtu <kahkonen@hotmail.com>
2022-06-08 13:01:02 +0300

    Finished 3_3

--- Exercises/Exercise 3/exercise3_3.py
+++ Exercises/Exercise 3/exercise3_3.py
@@ -82,9 +82,9 @@ def change_month(row):
 def fix_year(row):
     year = int(row[Col.YEAR.value])
     if year > 50:
-        return int(f"19{row['Year']}")
+        return int(f"19{row[Col.YEAR.value]}")
     else:
-        return int(f"20{row['Year']}")
+        return int(f"20{row[Col.YEAR.value]}")
 
 #split the original date into two columns:
 df[[Col.MONTH.value, Col.YEAR.value]] = df[Col.MONTH.value].str.split(
@@ -97,12 +97,52 @@ df[Col.MONTH.value] = df.apply(change_month, axis=1)
 df[Col.YEAR.value] = df.apply(fix_year, axis=1)
 
 
+# After the cleanups, create a correlation matrix of the data.
+df_corr = df.corr()
 
-#• After the cleanups, create a correlation matrix of the data. 
-#  Create a heatmap on the correlations as well. 
-#  
+#  Create a heatmap on the correlations as well.
+plt.clf()
+sb.heatmap(df_corr)
+plt.figure()
+  
 #  Which grocery stands out? (i.e. there seems to be one grocery 
 #  item whose price doesn't follow other groceries at all)
+ex1_sol_a = '''SOLUTION: Sunflower-oil seems to have no correlation with any
+other products price
+'''
 
 #• Which groceries seem to correlate to each other's prices? What 
 #  do they have in common
+ex_sol_b = '''SOLUTION: Wheat, Barley and Corn
+
+They are all harvested seeds of grasses grown at similar conditions and thus
+their harvest is dependant to the weather conditions good or bad
+
+Similar, yet lesser correlation is perceived with other such harvested produce
+'''
+
+# Bonus task:
+
+# Sometimes data can also reflect history! For example, "Corn"
+# and some other foods seem to have a notable peak in its price 
+# in one of the years. Find this year, and Google if you can find a 
+# reason for the price peak (for example, search: "us corn 
+# expensive XXXX". Can you find any other food that has relation 
+# to a real world event in a similar in this dataset?
+
+ex_sol_bonus = '''SOLUTION: Corn has several peaks in price, for example 
+a significant one in 2012 due to historic draught
+
+"With favorable conditions, farmers planted at the fastest pace
+ in U.S. history and planted the largest acreage in the past 75 years.
+ But historic drought conditions in most of the corn-growing states
+ caused the corn crop conditions to decline rapidly"
+
+Another one: Coffee had a dramatic increase in price in 1994 and 1997 due to
+crops flash freezing due to extremely strong El Nino effects in Southern
+America in the 90s
+
+Also I suspect the fact that this even also coincides right in the middle of
+of Starbuck's globalization timing had its effect as a sudden global increase
+in coffee demand, and thus also placed even more pressure for the price to increase
+'''
